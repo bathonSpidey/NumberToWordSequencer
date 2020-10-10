20c2f74e28e13818f652518d62769e6f3436e7d5
Abir Bhattacharyya <46240907+bathonSpidey@users.noreply.github.com>
2020-10-10 22:10:19 +0200

    Added more tested and genralized the code
    Refactored, added more test cases, and tested for special cases looks good

--- NumberToWordSequencer.py
+++ NumberToWordSequencer.py
@@ -6,32 +6,51 @@ Created on Fri Oct  9 22:50:40 2020
 """
 
 import unittest
+import re
 
 def GenerateSequences(sequence):
     generatedWordList=[]
-    word=IndividualCharacterCombination(sequence)
-    generatedWordList.append(word)
-    firstTwoCombined=chr(int(sequence[:2])+96)
-    firstTwoCombined+=IndividualCharacterCombination(sequence[2:])
-    generatedWordList.append(firstTwoCombined)
-    middleTwoCombined=IndividualCharacterCombination(sequence[0])
-    middleTwoCombined+=chr(int(sequence[1:3])+96)
-    middleTwoCombined+=IndividualCharacterCombination(sequence[3])
-    generatedWordList.append(middleTwoCombined)
-    return generatedWordList
+    generatedWordList.append(IndividualCharacterCombination(sequence))
+    generatedWordList= SlideTwoAtATime(sequence, generatedWordList)
+    generatedWordList.append(CombineTwoAtATime(sequence))
+    filteredList=FilterValidStrings(generatedWordList)
+    return filteredList
 
+def SlideTwoAtATime(sequence, generatedList):
+    for i in range(len(sequence)-1):
+        if i ==0:
+            firstHalf=chr(int(sequence[i:i+2])+96)
+            firstHalf+=IndividualCharacterCombination(sequence[2:])
+            generatedList.append(firstHalf)
+        elif i==len(sequence)-2:
+            lastHalf=IndividualCharacterCombination(sequence[:i])
+            lastHalf+=chr(int(sequence[i:i+2])+96)
+            generatedList.append(lastHalf)
+        else:
+            firstSequence=IndividualCharacterCombination(sequence[:i])
+            firstSequence+=chr(int(sequence[i:i+2])+96)
+            firstSequence+=IndividualCharacterCombination(sequence[i+2:])
+            generatedList.append(firstSequence)
+    return generatedList
+            
 def IndividualCharacterCombination(sequence):
     word=""
     for i in sequence:
         word+=chr(int(i)+96)
     return word
+
+def CombineTwoAtATime(sequence):
+        word=""
+        for i in range(0,len(sequence),2):
+            word+=chr(int(sequence[i:i+2])+96)
+        return word
+
+def FilterValidStrings(generatedWordList):
+    for word in generatedWordList:
+        if re.findall('[^A-Za-z]',word):
+            generatedWordList.remove(word)
+    return generatedWordList
     
-        
-
-
-
-
-
 
 class TestStringMethods(unittest.TestCase):
 
@@ -46,6 +65,18 @@ class TestStringMethods(unittest.TestCase):
     def test_nextTwoLettersCombined(self):
         wordList=GenerateSequences("1123")
         self.assertEqual(wordList[2], "alc")
-            
+    
+    def test_lastTwoLettersCombined(self):
+        wordList=GenerateSequences("1123")
+        self.assertEqual(wordList[3],"aaw")
+        
+    def test_lastTwoLettersCombined(self):
+        wordList=GenerateSequences("1123")
+        self.assertEqual(wordList[4],"kw")
+    
+    def test_specialCharacterWordsShouldBeRemoved(self):
+        wordList=GenerateSequences("11231")
+        self.assertEqual(len(wordList),5)
+
 if __name__ == '__main__':
     unittest.main()
